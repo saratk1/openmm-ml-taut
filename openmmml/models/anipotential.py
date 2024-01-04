a221b8442f6995b973329d16c1a10122ce22f953
saratk1 <sara.tkaczyk@gmail.com>
2024-01-04 11:40:33 +0100

    keep original code

--- openmmml/models/anipotential.py
+++ openmmml/models/anipotential.py
@@ -32,7 +32,6 @@ USE OR OTHER DEALINGS IN THE SOFTWARE.
 from openmmml.mlpotential import MLPotential, MLPotentialImpl, MLPotentialImplFactory
 import openmm
 from typing import Iterable, Optional, Union
-import torch 
 
 class ANIPotentialImplFactory(MLPotentialImplFactory):
     """This is the factory that creates ANIPotentialImpl objects."""
@@ -42,6 +41,117 @@ class ANIPotentialImplFactory(MLPotentialImplFactory):
 
 
 class ANIPotentialImpl(MLPotentialImpl):
+    """This is the MLPotentialImpl implementing the ANI potential.
+
+    The potential is implemented using TorchANI to build a PyTorch model.  A
+    TorchForce is used to add it to the OpenMM System.  The ANI1ccx and ANI2x
+    versions are currently supported.
+
+    Both ANI1ccx and ANI2x are ensembles of eight models.  Averaging over all eight
+    models leads to slightly more accurate results than any one individually.  You
+    can optionally use only a single model by specifying the modelIndex argument to
+    select which one to use.  This leads to a large improvement in speed, at the
+    cost of a small decrease in accuracy.
+
+    TorchForce requires the model to be saved to disk in a separate file.  By default
+    it writes a file called 'animodel.pt' in the current working directory.  You can
+    use the filename argument to specify a different name.  For example,
+
+    >>> system = potential.createSystem(topology, filename='mymodel.pt')
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+
+    def addForces(self,
+                  topology: openmm.app.Topology,
+                  system: openmm.System,
+                  atoms: Optional[Iterable[int]],
+                  forceGroup: int,
+                  filename: str = 'animodel.pt',
+                  implementation: str = 'nnpops',
+                  modelIndex: Optional[int] = None,
+                  **args):
+        # Create the TorchANI model.
+
+        import torchani
+        import torch
+        import openmmtorch
+
+        # `nnpops` throws error if `periodic_table_index`=False if one passes `species` as `species_to_tensor` from `element`
+        _kwarg_dict = {'periodic_table_index': True}
+        if self.name == 'ani1ccx':
+            model = torchani.models.ANI1ccx(**_kwarg_dict)
+        elif self.name == 'ani2x':
+            model = torchani.models.ANI2x(**_kwarg_dict)
+        else:
+            raise ValueError('Unsupported ANI model: '+self.name)
+        if modelIndex is not None:
+            model = model[modelIndex]
+
+        # Create the PyTorch model that will be invoked by OpenMM.
+        includedAtoms = list(topology.atoms())
+        if atoms is not None:
+            includedAtoms = [includedAtoms[i] for i in atoms]
+        species = torch.tensor([[atom.element.atomic_number for atom in includedAtoms]])
+        if implementation == 'nnpops':
+            try:
+                from NNPOps import OptimizedTorchANI
+                model = OptimizedTorchANI(model, species)
+            except Exception as e:
+                print(f"failed to equip `nnpops` with error: {e}")
+        elif implementation == "torchani":
+            pass # do nothing
+        else:
+            raise NotImplementedError(f"implementation {implementation} is not supported")
+
+        class ANIForce(torch.nn.Module):
+
+            def __init__(self, model, species, atoms, periodic):
+                super(ANIForce, self).__init__()
+                self.model = model
+                self.species = torch.nn.Parameter(species, requires_grad=False)
+                self.energyScale = torchani.units.hartree2kjoulemol(1)
+                if atoms is None:
+                    self.indices = None
+                else:
+                    self.indices = torch.tensor(sorted(atoms), dtype=torch.int64)
+                if periodic:
+                    self.pbc = torch.nn.Parameter(torch.tensor([True, True, True], dtype=torch.bool), requires_grad=False)
+                else:
+                    self.pbc = None
+
+            def forward(self, positions, boxvectors: Optional[torch.Tensor] = None):
+                positions = positions.to(torch.float32)
+                #print(f"(boxvectors, scale): {boxvectors, scale}")
+                if self.indices is not None:
+                    positions = positions[self.indices]
+                if boxvectors is None:
+                    _, energy = self.model((self.species, 10.0*positions.unsqueeze(0)))
+                else:
+                    boxvectors = boxvectors.to(torch.float32)
+                    _, energy = self.model((self.species, 10.0*positions.unsqueeze(0)), cell=10.0*boxvectors, pbc=self.pbc)
+
+                return self.energyScale*energy
+
+        # is_periodic...
+        is_periodic = (topology.getPeriodicBoxVectors() is not None) or system.usesPeriodicBoundaryConditions()
+        aniForce = ANIForce(model, species, atoms, is_periodic)
+
+        # Convert it to TorchScript and save it.
+
+        module = torch.jit.script(aniForce)
+        module.save(filename)
+
+        # Create the TorchForce and add it to the System.
+
+        force = openmmtorch.TorchForce(filename)
+        force.setForceGroup(forceGroup)
+        force.setUsesPeriodicBoundaryConditions(is_periodic)
+        system.addForce(force)
+
+class TautANIPotentialImpl(MLPotentialImpl):
     """This is the MLPotentialImpl implementing the ANI potential.
 
     The potential is implemented using TorchANI to build a PyTorch model.  A
@@ -182,3 +292,4 @@ class ANIPotentialImpl(MLPotentialImpl):
 
 MLPotential.registerImplFactory('ani1ccx', ANIPotentialImplFactory())
 MLPotential.registerImplFactory('ani2x', ANIPotentialImplFactory())
+MLPotential.registerImplFactory('tautani2x', ANIPotentialImplFactory())
