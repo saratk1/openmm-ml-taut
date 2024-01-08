ecb9322f7db1c3b75eda97f07439b0724d842ae1
saratk1 <sara.tkaczyk@gmail.com>
2024-01-08 15:38:25 +0100

    fix argument pass & energy evaluation

--- openmmml/models/anipotential.py
+++ openmmml/models/anipotential.py
@@ -40,10 +40,10 @@ class ANIPotentialImplFactory(MLPotentialImplFactory):
     def createImpl(self, name: str, **args) -> MLPotentialImpl:
         if name.startswith("tau"):
             self.lambda_val = args["lambda_val"]
-            self.t1_indices = args["t1_indices"]
-            self.t2_indices = args["t2_indices"]
+            self.t2_idx_mask = args["t2_idx_mask"]
+            self.t2_idx_mask = args["t2_idx_mask"]
             name = "ani2x"
-            return TautANIPotentialImpl(name)
+            return TautANIPotentialImpl(name, **args)
         return ANIPotentialImpl(name)
 
 
@@ -193,8 +193,11 @@ class TautANIPotentialImpl(MLPotentialImpl):
     >>> system = potential.createSystem(topology, filename='mymodel.pt')
     """
 
-    def __init__(self, name):
+    def __init__(self, name, lambda_val=None, t1_idx_mask=None, t2_idx_mask=None):
         self.name = name
+        self.lambda_val = lambda_val
+        self.t1_idx_mask = t1_idx_mask
+        self.t2_idx_mask = t2_idx_mask
 
     def addForces(
         self,
@@ -250,28 +253,32 @@ class TautANIPotentialImpl(MLPotentialImpl):
                 species,
                 atoms,
                 periodic,
-                lambda_value,
-                t1_indices,
-                t2_indices,
+                lambda_val,
+                t1_idx_mask,
+                t2_idx_mask,
             ):
                 super(ANIForce, self).__init__()
                 self.model = model
                 self.species = torch.nn.Parameter(species, requires_grad=False)
 
-                mask = torch.nn.Parameter(t1_indices, requires_grad=False)
-                t2_species = torch.nn.Parameter(
+                # check which species should be masked
+                mask_t1 = torch.nn.Parameter(t1_idx_mask, requires_grad=False)
+                # first, get all species
+                self.t2_species = torch.nn.Parameter(
                     species.clone().detach(), requires_grad=False
                 )
-                t2_species[:, mask] = -1
+                # mask dummy atom (defining topology of tautomer 2) with -1
+                self.t2_species[:, mask_t1] = -1
 
-                mask = torch.nn.Parameter(t2_indices, requires_grad=False)
-                t1_species = torch.nn.Parameter(
+                # do the same for tautomer 2
+                mask_t2 = torch.nn.Parameter(t2_idx_mask, requires_grad=False)
+                self.t1_species = torch.nn.Parameter(
                     species.clone().detach(), requires_grad=False
                 )
-                t1_species[:, mask] = -1
+                self.t1_species[:, mask_t2] = -1
 
                 self.energyScale = torchani.units.hartree2kjoulemol(1)
-                self.lambda_value = lambda_value
+                self.lambda_val = lambda_val
 
                 if atoms is None:
                     self.indices = None
@@ -291,6 +298,7 @@ class TautANIPotentialImpl(MLPotentialImpl):
                 if self.indices is not None:
                     positions = positions[self.indices]
 
+                # combine species of tautomer 1 and tautomer 2
                 species_positions_tuple = (
                     torch.stack((self.t1_species, self.t2_species)).squeeze(),
                     10.0 * positions.repeat(2, 1).reshape(2, -1, 3),
@@ -301,17 +309,18 @@ class TautANIPotentialImpl(MLPotentialImpl):
                 else:
                     boxvectors = boxvectors.to(torch.float32)
                     _, energy = self.model(
-                        self.species,
                         species_positions_tuple,
                         cell=10.0 * boxvectors,
                         pbc=self.pbc,
                     )
 
+                # get the energy of tautomer 1 and tautomer 2
                 t1 = energy[0]
                 t2 = energy[1]
 
+                # interpolate between potential of tautomer 1 and tautomer 2 with lambda
                 return self.energyScale * (
-                    (self.lambda_value * t1) + ((1 - self.lambda_value) * t2)
+                    (self.lambda_val * t1) + ((1 - self.lambda_val) * t2)
                 )
 
         # is_periodic...
@@ -324,8 +333,8 @@ class TautANIPotentialImpl(MLPotentialImpl):
             atoms,
             is_periodic,
             self.lambda_val,
-            self.t1_indices,
-            self.t2_indices,
+            self.t1_idx_mask,
+            self.t2_idx_mask,
         )
 
         # Convert it to TorchScript and save it.
